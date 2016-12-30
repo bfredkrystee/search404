dd3b5320de2edb59e3a08c462d2979b045b384df
venu_gopal <venu_gopal@2346758.no-reply.drupal.org>
2016-12-30 10:00:38 +0530

    Issue #2839630 by venu_gopal: WARNING | t() calls should be avoided in classes, use dependency | | injection and $this->t() instead

--- src/Form/Search404Settings.php
+++ src/Form/Search404Settings.php
@@ -29,41 +29,41 @@ class Search404Settings extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['search404_jump'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Jump directly to the search result when there is only one result'),
-      '#description' => t('Works only with Core, Apache Solr, Lucene and Xapian searches. An HTTP status of 301 or 302 will be returned for this redirect.'),
+      '#title' => $this->t('Jump directly to the search result when there is only one result'),
+      '#description' => $this->t('Works only with Core, Apache Solr, Lucene and Xapian searches. An HTTP status of 301 or 302 will be returned for this redirect.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_jump'),
     );
     $form['search404_first'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Jump directly to the first search result even when there are multiple results'),
-      '#description' => t('Works only with Core, Apache Solr, Lucene and Xapian searches. An HTTP status of 301 or 302 will be returned for this redirect.'),
+      '#title' => $this->t('Jump directly to the first search result even when there are multiple results'),
+      '#description' => $this->t('Works only with Core, Apache Solr, Lucene and Xapian searches. An HTTP status of 301 or 302 will be returned for this redirect.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_first'),
     );
     $form['search404_do_google_cse'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Do a Google CSE Search instead of a Drupal Search when a 404 occurs'),
-      '#description' => t('Requires Google CSE and Google CSE Search modules to be enabled.'),
+      '#title' => $this->t('Do a Google CSE Search instead of a Drupal Search when a 404 occurs'),
+      '#description' => $this->t('Requires Google CSE and Google CSE Search modules to be enabled.'),
       '#attributes' => \Drupal::moduleHandler()->moduleExists('google_cse') ? array() : array('disabled' => 'disabled'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_do_google_cse'),
     );
     $form['search404_do_search_by_page'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Do a "Search by page" Search instead of a Drupal Search when a 404 occurs'),
-      '#description' => t('Requires "Search by page" module to be enabled.'),
+      '#title' => $this->t('Do a "Search by page" Search instead of a Drupal Search when a 404 occurs'),
+      '#description' => $this->t('Requires "Search by page" module to be enabled.'),
       '#attributes' => \Drupal::moduleHandler()->moduleExists('search_by_page') ? array() : array('disabled' => 'disabled'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_do_search_by_page'),
     );
     // Custom search path implementation.
     $form['search404_do_custom_search'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Do a "Search" with custom path instead of a Drupal Search when a 404 occurs'),
-      '#description' => t('Redirect the user to a Custom search path to be entered below. Can be used to open a view with path parameter.'),
+      '#title' => $this->t('Do a "Search" with custom path instead of a Drupal Search when a 404 occurs'),
+      '#description' => $this->t('Redirect the user to a Custom search path to be entered below. Can be used to open a view with path parameter.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_do_custom_search'),
     );
     $form['search404_custom_search_path'] = array(
       '#type' => 'textfield',
-      '#title' => t('Custom search path'),
-      '#description' => t('The custom search path: example: myownsearch/@keys. The token "@keys" will be replaced with the search keys from the URL.'),
+      '#title' => $this->t('Custom search path'),
+      '#description' => $this->t('The custom search path: example: myownsearch/@keys. The token "@keys" will be replaced with the search keys from the URL.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_custom_search_path'),
     );
     // Added for having a 301 redirect instead of the standard 302
@@ -71,103 +71,103 @@ class Search404Settings extends ConfigFormBase {
     // Lucene and Xapian. Can this even be done? Meta refresh?
     $form['search404_redirect_301'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Use a 301 Redirect instead of 302 Redirect'),
-      '#description' => t('This applies when the option to jump to first result is enabled and also for search404 results pages other than for Core, Apache Solr, Lucene and Xapian.'),
+      '#title' => $this->t('Use a 301 Redirect instead of 302 Redirect'),
+      '#description' => $this->t('This applies when the option to jump to first result is enabled and also for search404 results pages other than for Core, Apache Solr, Lucene and Xapian.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_redirect_301'),
     );
     // Added for preventing automatic search for large sites.
     $form['search404_skip_auto_search'] = array(
-      '#title' => t('Disable auto search'),
-      '#description' => t('Disable automatically searching for the keywords when a page is not found and instead show the populated search form with the keywords. Useful for large sites to reduce server loads.'),
+      '#title' => $this->t('Disable auto search'),
+      '#description' => $this->t('Disable automatically searching for the keywords when a page is not found and instead show the populated search form with the keywords. Useful for large sites to reduce server loads.'),
       '#type' => 'checkbox',
       '#default_value' => \Drupal::config('search404.settings')->get('search404_skip_auto_search'),
     );
     // Disable the drupal error message when showing search results.
     $form['search404_disable_error_message'] = array(
-      '#title' => t('Disable error message'),
+      '#title' => $this->t('Disable error message'),
       '#type' => 'checkbox',
-      '#description' => t('Disable the Drupal error message when search results are shown on a 404 page.'),
+      '#description' => $this->t('Disable the Drupal error message when search results are shown on a 404 page.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_disable_error_message'),
     );
 
     // To add custom error message.
     $form['search404_custom_error_message'] = array(
-      '#title' => t('Custom error message'),
+      '#title' => $this->t('Custom error message'),
       '#type' => 'textfield',
       '#placeholder' => 'For example, Invalid search for @keys, Sorry the page does not exist, etc.',
-      '#description' => t('A custom error message instead of default Drupal message, that should be displayed when search results are shown on a 404 page, use "@keys" to insert the searched key value if necessary.'),
+      '#description' => $this->t('A custom error message instead of default Drupal message, that should be displayed when search results are shown on a 404 page, use "@keys" to insert the searched key value if necessary.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_custom_error_message'),
     );
 
     $form['advanced'] = array(
       '#type' => 'fieldset',
-      '#title' => t('Advanced settings'),
+      '#title' => $this->t('Advanced settings'),
       '#collapsible' => TRUE,
       '#collapsed' => TRUE,
     );
     $form['advanced']['search404_use_or'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Use OR between keywords when searching'),
+      '#title' => $this->t('Use OR between keywords when searching'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_use_or'),
     );
     $form['advanced']['search404_use_search_engine'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Use auto-detection of keywords from search engine referer'),
-      '#description' => t('This feature will conduct a search based on the query string got from a search engine if the URL of the search result points to a 404 page in the current website. Currently supported search engines: Google, Yahoo, Altavista, Lycos, Bing and AOL.'),
+      '#title' => $this->t('Use auto-detection of keywords from search engine referer'),
+      '#description' => $this->t('This feature will conduct a search based on the query string got from a search engine if the URL of the search result points to a 404 page in the current website. Currently supported search engines: Google, Yahoo, Altavista, Lycos, Bing and AOL.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_use_search_engine'),
     );
     $form['advanced']['search404_ignore'] = array(
       '#type' => 'textarea',
-      '#title' => t('Words to ignore'),
-      '#description' => t('These words will be ignored from the search query. Separate words with a space, e.g.: "and or the".'),
+      '#title' => $this->t('Words to ignore'),
+      '#description' => $this->t('These words will be ignored from the search query. Separate words with a space, e.g.: "and or the".'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_ignore'),
     );
     $form['advanced']['search404_ignore_paths'] = array(
       '#type' => 'textarea',
-      '#title' => t('Specific paths to ignore'),
-      '#description' => t('These paths will be ignored. Site default "Page not found" page will be displayed. Enter one path per line. The "*" character is a wildcard. Example paths are blog for the blog page and blog/* for every personal blog.'),
+      '#title' => $this->t('Specific paths to ignore'),
+      '#description' => $this->t('These paths will be ignored. Site default "Page not found" page will be displayed. Enter one path per line. The "*" character is a wildcard. Example paths are blog for the blog page and blog/* for every personal blog.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_ignore_paths', ''),
     );
     $form['advanced']['search404_ignore_extensions'] = array(
       '#type' => 'textfield',
-      '#title' => t('Extensions to ignore'),
-      '#description' => t('These extensions will be ignored from the search query, e.g.: http://www.example.com/invalid/page.php will only search for "invalid page". Separate extensions with a space, e.g.: "htm html php". Do not include leading dot.'),
+      '#title' => $this->t('Extensions to ignore'),
+      '#description' => $this->t('These extensions will be ignored from the search query, e.g.: http://www.example.com/invalid/page.php will only search for "invalid page". Separate extensions with a space, e.g.: "htm html php". Do not include leading dot.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_ignore_extensions'),
     );
     $form['advanced']['search404_ignore_query'] = array(
       '#type' => 'textfield',
-      '#title' => t('Extensions to abort search'),
-      '#description' => t('A search will not be performed for a query ending in these extensions. Separate extensions with a space, e.g.: "gif jpg jpeg bmp png". Do not include leading dot.'),
+      '#title' => $this->t('Extensions to abort search'),
+      '#description' => $this->t('A search will not be performed for a query ending in these extensions. Separate extensions with a space, e.g.: "gif jpg jpeg bmp png". Do not include leading dot.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_ignore_query'),
     );
     $form['advanced']['search404_regex'] = array(
       '#type' => 'textfield',
-      '#title' => t('PCRE filter'),
-      '#description' => t('This regular expression will be applied to filter all queries. The parts of the path that match the expression will be EXCLUDED from the search. You do NOT have to enclose the regex in forward slashes when defining the PCRE. e.g.: use "[foo]bar" instead of "/[foo]bar/". On how to use a PCRE Regex please refer <a href="http://php.net/pcre">PCRE pages in the PHP Manual</a>.'),
+      '#title' => $this->t('PCRE filter'),
+      '#description' => $this->t('This regular expression will be applied to filter all queries. The parts of the path that match the expression will be EXCLUDED from the search. You do NOT have to enclose the regex in forward slashes when defining the PCRE. e.g.: use "[foo]bar" instead of "/[foo]bar/". On how to use a PCRE Regex please refer <a href="http://php.net/pcre">PCRE pages in the PHP Manual</a>.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_regex'),
     );
     // Show custom title for the 404 search results page.
     $form['advanced']['search404_page_title'] = array(
       '#type' => 'textfield',
-      '#title' => t('Custom page title'),
-      '#description' => t('You can enter a value that will displayed at the title of the webpage e.g. "Page not found".'),
+      '#title' => $this->t('Custom page title'),
+      '#description' => $this->t('You can enter a value that will displayed at the title of the webpage e.g. "Page not found".'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_page_title'),
     );
     // Show custom text below the search form for the 404 search
     // results page.
     $form['advanced']['search404_page_text'] = array(
       '#type' => 'textarea',
-      '#title' => t('Custom page text'),
+      '#title' => $this->t('Custom page text'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_page_text'),
-      '#description' => t('You can enter a custom text message that can be displayed at the top of the search results, HTML formatting can be used.'),
+      '#description' => $this->t('You can enter a custom text message that can be displayed at the top of the search results, HTML formatting can be used.'),
     );
 
     // Add a redirect url option for handling empty results display.
     $form['advanced']['search404_page_redirect'] = array(
-      '#title' => t('Add a redirection url for empty search results.'),
+      '#title' => $this->t('Add a redirection url for empty search results.'),
       '#type' => 'textfield',
       '#placeholder' => 'For example, /node, /node/10, etc.',
-      '#description' => t('You can enter a valid url with a leading "/" to display instead of empty result.'),
+      '#descriptionc' => $this->t('You can enter a valid url with a leading "/" to display instead of empty result.'),
       '#default_value' => \Drupal::config('search404.settings')->get('search404_page_redirect'),
     );
 
@@ -182,7 +182,7 @@ class Search404Settings extends ConfigFormBase {
       '#type' => 'markup',
       '#prefix' => '<p>',
       '#suffix' => '</p>',
-      '#value' => t('Saving this form will revert the 404 handling on the site to this module.'),
+      '#value' => $this->t('Saving this form will revert the 404 handling on the site to this module.'),
     );
 
     return parent::buildForm($form, $form_state);
